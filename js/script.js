74aae929bc37baed6ce62597c8b370457209c187
Aziza Jurabaeva <azizajurabaeva@gmail.com>
2019-09-17 14:25:39 -0400

    done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// function myFunction(x) {
+//     x.classList.toggle("change");
+//   }
+
+//   function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//   }
+
+
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+    
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
